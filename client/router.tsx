c71af56857421ef7f322a1f0824e2dbd799d1590
Jackson Simpson <jackson7890air@gmail.com>
2023-07-20 14:45:53 +1200

    Add contact page

--- client/router.tsx
+++ client/router.tsx
@@ -8,6 +8,7 @@ import App from './components/App'
 import Home from './components/Home'
 import About from './components/About'
 import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -15,6 +16,7 @@ export const router = createBrowserRouter(
       <Route index element={<Home />} />
       <Route path="/about-me" element={<About />} />
       <Route path="/projects" element={<Projects />} />
+      <Route path="/contact" element={<Contact />} />
     </Route>
   )
 )
